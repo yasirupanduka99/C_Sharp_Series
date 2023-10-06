add9859e98babb256befc86321eb45a6e80f7e8a
Yasiru Panduka <it20162382@my.sliit.lk>
2023-10-06 22:17:14 +0530

    first commit

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args) //this is main method. which is mainly first runing method on a C sharp program.
+        {
+            Console.WriteLine("Hello World!"); //WriteLine will print that string and move down cursor to below line.
+            Console.ReadLine(); //this ReadLine() execute 
+        }
+    }
+}
