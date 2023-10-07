9a25d6ce0f1d7707b534390cd0a291e7a774336c
Yasiru Panduka <it20162382@my.sliit.lk>
2023-10-07 19:47:27 +0530

    C# casting | convertion between two data types

--- C#_05_Variable_DataTypes/Program.cs
+++ C#_05_Variable_DataTypes/Program.cs
@@ -5,7 +5,31 @@
     {
         static void Main(string[] args)
         {
+            /*
+            Datatypes are 
+            string - string use for words, 
+            char - char use for single letter or character, 
+            int - integer contain numbers, 
+            long - long contain long number of numbers, 
+            float - this contain decimal numbers, 
+            double - double also contain decimal numbers but can contain long numbers for decimal, 
+            bool - bool contain true or false
+            */
 
+            //variable declaration
+            string name; //use for word
+
+            char fuelTypes; //use for single letter or character
+
+            int age; //use for whole numbers but use middiume range of numbers
+
+            long fullName; //use for whole number but long range or numbers
+
+            float salary; //use for decimals
+
+            double saveMoney; // use for decimal but long range decimals
+
+            bool maritalStatus; //True or false
         }
     }
 
---
+++ C#_06_Casting/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace C_sharp_casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n\n------------ C# Casting | Converting DataTypes ------------\n\n");
+
+            //casting means convert one data type to another data type.
+            //there are two types of casting ---> 1.implicit casting (Automaticaly casting)  2. Explicit casting (Manual casting)
+
+
+            // ----- 1.implicit casting | automatic casting ----
+            int number; //variable declaration
+            number = 10; //variable initilition
+
+            int number2 = 20; //this is variable declaration and initialization
+
+            double numberConvertToDouble = number2; //this is automatic casting / implicit casting. you can cast int to double automatically because that both can contain numbers.
+
+            Console.WriteLine("This is integer no : " + number2);
+            Console.WriteLine("This is converted no of integer to double : " + numberConvertToDouble);
+
+
+
+            // ---- 2.explicit casting | manual casting ----
+            double number3 = 10.5;
+
+            int numberConvertedToInt = (int)number3; //cast to integer
+            string numberConvertedToString = number3.ToString(); //cast to string
+
+            Console.WriteLine("\nThis is integer no : " + number3); //this '\n' can cut down a line
+            Console.WriteLine("This is converted no of double to integer : " + numberConvertedToInt);
+            Console.WriteLine("This converted no of double to string : " + numberConvertedToString);
+
+
+            Console.ReadLine();
+        }
+    }
+}
