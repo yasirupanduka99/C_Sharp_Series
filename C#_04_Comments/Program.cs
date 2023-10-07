2b3976285a8066a19dc6787c10a6d51454d8567e
Yasiru Panduka <it20162382@my.sliit.lk>
2023-10-07 18:09:08 +0530

    C# Comments

---
+++ C#_04_Comments/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace C_Sharp_04_Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is testing for C# comments");
+            Console.ReadLine();
+        }
+    }
+}
+
