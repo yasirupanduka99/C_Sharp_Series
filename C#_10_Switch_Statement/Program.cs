8d408281a57058f984408ea415d18056f7e95560
Yasiru Panduka <it20162382@my.sliit.lk>
2023-10-08 23:51:07 +0530

    Start C# switch statements

---
+++ C#_10_Switch_Statement/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace C_Sharp_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n ------------ Switch Statement | Conditional Statement ------------ \n");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
