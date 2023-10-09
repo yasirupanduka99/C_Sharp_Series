f8f532a1dcfc0db16aa1c42370046bc653399e00
Yasiru Panduka <it20162382@my.sliit.lk>
2023-10-09 23:39:17 +0530

    C# Switch Statement :

--- C#_10_Switch_Statement/Program.cs
+++ C#_10_Switch_Statement/Program.cs
@@ -8,6 +8,65 @@
 
             //Switch statement is also a type of conditional statemnt
 
+            Console.Write("Enter Number (1 to 7 only allowed) : ");
+            int day = Int16.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1: 
+                    Console.WriteLine("No 1 is Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("No 2 is Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("No 3 is Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("No 4 is Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("No 5 is Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("No 6 is Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("No 7 is Sunday");
+                    break;
+                default: 
+                    Console.WriteLine("Invalid Entered Number");
+                    break;
+            }
+
+
+            Console.Write("\nType 'Even' or 'Odd' : ");
+            string selection = Console.ReadLine();
+
+            switch (selection)
+            {
+                case "even":
+                    Console.WriteLine("2, 4, 6, 8, 10");
+                    break;
+                case "Even":
+                    Console.WriteLine("2, 4, 6, 8, 10");
+                    break;
+                case "EVEN":
+                    Console.WriteLine("2, 4, 6, 8, 10");
+                    break;
+                case "odd":
+                    Console.WriteLine("1, 3, 5, 7, 9");
+                    break;
+                case "Odd":
+                    Console.WriteLine("1, 3, 5, 7, 9");
+                    break;
+                case "ODD":
+                    Console.WriteLine("1, 3, 5, 7, 9");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input!!");
+                    break;
+            }
 
 
 
