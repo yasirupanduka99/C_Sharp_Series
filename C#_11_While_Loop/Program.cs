d9d532f6497c4c3934c112805435782d589c6dde
Yasiru Panduka <it20162382@my.sliit.lk>
2023-10-09 23:58:48 +0530

    changes adding

--- C#_11_While_Loop/Program.cs
+++ C#_11_While_Loop/Program.cs
@@ -14,6 +14,23 @@
             }
 
 
+            int StCount = 1;
+            double mark;
+            double total = 0;
+            double average;
+
+            while (StCount <= 10)
+            {
+                Console.Write("Enter Marks of Student " + StCount + " : ");
+                mark = Double.Parse(Console.ReadLine());
+
+                total = total + mark;
+            }
+
+            average = total/10;
+            Console.WriteLine("Total is : " + total);
+
+
 
             Console.ReadLine();
         }
