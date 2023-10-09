a189a1e9be89319cfec973bf08eae5064cd70eab
Yasiru Panduka <it20162382@my.sliit.lk>
2023-10-09 23:53:07 +0530

    Start Iteration in C# 😁✨

---
+++ C#_11_While_Loop/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace C_Sharp_While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n ------------ While Loop Statement | Iteration ------------ \n");
+
+            int count = 1;
+
+            while (count <= 100) {
+                Console.WriteLine(count);
+                count++;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
