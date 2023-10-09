b0da48d842381da9e9ef3b16df9aefac3aa25016
Yasiru Panduka <it20162382@my.sliit.lk>
2023-10-10 00:08:47 +0530

    C#_While_Loop_statement 👻❤️🪶

--- C#_11_While_Loop/Program.cs
+++ C#_11_While_Loop/Program.cs
@@ -4,14 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n ------------ While Loop Statement | Iteration ------------ \n");
+            Console.WriteLine("\n ------------ While Loop Statement | Iteration ------------ \n\n");
 
             int count = 1;
 
-            while (count <= 100) {
+            while (count <= 5) {
                 Console.WriteLine(count);
                 count++;
             }
+
+            Console.WriteLine("\n\n");
 
 
             int StCount = 1;
@@ -25,10 +27,13 @@
                 mark = Double.Parse(Console.ReadLine());
 
                 total = total + mark;
+                StCount++;
             }
 
             average = total/10;
-            Console.WriteLine("Total is : " + total);
+            Console.WriteLine("\nTotal Students : " + (StCount-1));
+            Console.WriteLine("Total Marks    : " + total);
+            Console.WriteLine("Average Marks  : " + average);
 
 
 
