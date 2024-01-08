44b09250d469d53e3c3ecda9d50d45bece66a443
Yasiru Panduka <it20162382@my.sliit.lk>
2024-01-08 17:01:46 +0530

    C Sharp For Loop Done and Dusted! 👻🍟

---
+++ C#_13_For_Loop/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace C_Sharp_For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n ------------ For Loop Statement | Iteration ------------ \n\n");
+
+            double marks;
+            double total = 0;
+            double average = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("Enter Marks Of Student's Subject " + i + " : ");
+                marks = double.Parse(Console.ReadLine());
+
+                total = total + marks;
+                average = total / i;
+            }
+
+            Console.WriteLine("\n/n ------------ Results ------------ \n");
+
+            Console.WriteLine("Total Marks of Student : " + total);
+            Console.WriteLine("Average Marks of Student  : " + average);
+
+            Console.ReadLine();
+        }
+    }
+}
