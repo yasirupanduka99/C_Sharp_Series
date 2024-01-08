df065d6ccde4f45b870ab402481d92f2ad134bb9
Yasiru Panduka <it20162382@my.sliit.lk>
2024-01-08 17:32:41 +0530

    C#_Break and Continue Statement Done and Dusted! 🤗🍟🪶

---
+++ C#_14_Break and Continue Statements/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace C_Sharp_Break_and_Continue_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n ------------ Break and Continue Statements ------------ \n\n");
+
+            Console.WriteLine("----Break Statement----");
+            for(int i = 0; i <10; i++)
+            {
+                if(i == 4)
+                {
+                    break; //when i = 4 then loop will break;
+                }
+
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\n----Continue Statement----");
+            for (int i = 0; i <= 10; i++)
+            {
+                if(i == 4)
+                {
+                    continue; //when comes to this statement code will continue but dosent go to console.writeline statement instead of its go to for loop. so 4 not will show in output.
+                }
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
