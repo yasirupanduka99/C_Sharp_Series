d132bb367d577450097f838604a77fa49e3ab0a2
Yasiru Panduka <it20162382@my.sliit.lk>
2024-01-09 17:39:19 +0530

    C# OOP constructors and methods done and dusted! 🪶❤️✨

---
+++ C#_18_OOP_Constructors_and_Methods/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_constructors_and_methods
+{
+    class Person
+    {
+
+        public string name;
+        public int age;
+
+        public string subject;
+        public int grade;
+
+        //Constructors
+        // 1 - Default Constructor - when program initiate this deafult constructor gives values for the properties.
+        public Person()
+        {
+
+        }
+
+        // 2 - Overloaded Constructor
+        public Person(string subject, int grade)
+        {
+            this.grade = grade;
+            this.subject = subject;
+        }
+
+        public void studying()
+        {
+            Console.WriteLine("Studying Method Triggered!");
+        }
+    }
+}
---
+++ C#_18_OOP_Constructors_and_Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace C_Sharp_constructors_and_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n ------------ Object Oriented Programming | Constructors and Methods ------------ \n\n");
+
+            //Object person1 with default constructor
+            Person person1 = new Person();
+
+            //Object person2 with Overloaded Constructor
+            Person person2 = new Person("Maths", 75);
+
+            Console.WriteLine("Subject : " + person2.subject);
+            Console.WriteLine("Grade   : " + person2.grade);
+
+            person1.studying(); //calling the method or invoking the method
+
+            Console.ReadLine();
+        }
+    }
+}
